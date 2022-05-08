510dc30bcc2ebe79ddb784d0ee8ec2662a0fe5a3
undcybele <andreea1bolea@gmail.com>
2022-05-08 19:37:21 +0300

    Updated UI for account

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {IUser} from "../../models/IUser";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-account',
@@ -6,11 +9,20 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  userDetails!: IUser
 
-  constructor() {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
   }
 
   ngOnInit(): void {
+    this.userDetails = this.authService.getLoggedUser
+    console.log(this.userDetails)
   }
 
+  signOut() {
+    this.authService.SignOut().then(() => this.router.navigate(['/auth']))
+  }
 }
