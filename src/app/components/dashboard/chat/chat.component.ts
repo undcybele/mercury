ecbe3d08dab876f5c2d571dad5e48e758706de38
undcybele <andreea1bolea@gmail.com>
2022-05-08 19:38:42 +0300

    Updated UI for chat

---
+++ src/app/components/dashboard/chat/chat.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  chatroom = "Chat room 1"
+  messages: any[] = [
+    {
+      text: 'Hello',
+      date: new Date(),
+      reply: false,
+      user: {
+        name: 'Bot',
+        avatar: 'https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/robot-face.png',
+      },
+    },
+  ];
+
+  sendMessage(event: { message: any; }) {
+    this.messages.push({
+      text: event.message,
+      date: new Date(),
+      reply: true,
+      user: {
+        name: 'John Doe',
+        avatar: 'https://techcrunch.com/wp-content/uploads/2015/08/safe_image.gif',
+      },
+    });
+  }
+
+}
