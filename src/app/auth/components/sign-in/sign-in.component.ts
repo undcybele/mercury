de06b7d31e9ad9dbf59f610ae56b42173e97b67c
I551967 <andreea.bolea@sap.com>
2022-05-11 19:42:51 +0300

    Updated sign in page ui

--- src/app/auth/components/sign-in/sign-in.component.ts
+++ src/app/auth/components/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../auth.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sign-in',
@@ -7,14 +9,27 @@ import {AuthService} from "../../auth.service";
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  form!: FormGroup;
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
   ) {
+    this.form = fb.group({email: null, password: null})
   }
 
   ngOnInit(): void {
   }
+
+  signIn(email: string, pass: string) {
+      this.authService.SignIn(email, pass).then()
+  }
+
+  googleAuth() {
+    this.authService.GoogleAuth().then()
+  }
+
 
 
 }
