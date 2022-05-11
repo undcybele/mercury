66777f0c789bdc428f7960ccbfc3ecfd038c87fe
I551967 <andreea.bolea@sap.com>
2022-05-11 19:43:29 +0300

    Updated sign up page ui

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../auth.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {NbToastrService} from "@nebular/theme";
 
 @Component({
   selector: 'app-sign-up',
@@ -7,13 +10,26 @@ import {AuthService} from "../../auth.service";
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  form!: FormGroup;
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+    private toastService: NbToastrService
   ) {
+    this.form = fb.group({email: null, password: null})
   }
 
   ngOnInit(): void {
+  }
+
+  signUp(email: string, pass: string){
+
+  }
+
+  googleAuth() {
+    this.authService.GoogleAuth().then()
   }
 
 }
