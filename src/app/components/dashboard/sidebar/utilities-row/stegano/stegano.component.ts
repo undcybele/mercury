c3607fbeb1c96c666913920d0c74e79bff82b2d1
undcybele <andreea1bolea@gmail.com>
2022-06-06 19:46:37 +0300

    Updated stegano to download image from anchor

--- src/app/components/dashboard/sidebar/utilities-row/stegano/stegano.component.ts
+++ src/app/components/dashboard/sidebar/utilities-row/stegano/stegano.component.ts
@@ -37,34 +37,35 @@ export class SteganoComponent implements OnInit {
     const text = this.form.value.text
     const pass = this.form.value.password
     const reader = new FileReader()
-    const toast = (msg: string, title: string) => {showToast(this.toastService, msg, title, 'danger')}
+    const toast = (msg: string, title: string) => {
+      showToast(this.toastService, msg, title, 'danger')
+    }
     // @ts-ignore
     let f = document.getElementById('upload_file').files[0]
     if (f) {
       reader.onloadend = function (e) {
-        try {
-          hasCapacity(<ArrayBuffer>e.target!.result, text)
-        }
-        catch {
+        if (!hasCapacity(<ArrayBuffer>e.target!.result, text))
           toast("This message is too big for this file", "Not enough space!")
-        }
-        try {
-          isPNG(f.name)
-        } catch {
+        if (!isPNG(f.name))
           toast("The image must be of PNG format and of RGB type", "Bad image!")
-        }
-        const newImage = document.createElement("img")
-        newImage.setAttribute("src", "data:image/png;base64," + encode(<ArrayBuffer>e.target!.result, text, pass))
-        document.getElementById('encoder-form')!.appendChild(newImage)
-        }
+
+        const steganoImage = "data:image/png;base64," + encode(<ArrayBuffer>e.target!.result, text, pass)
+        const link = document.createElement("a");
+        document.getElementById('encoder-form')!.appendChild(link)
+        link.text = 'Download stegano image'
+        link.href = steganoImage;
+        link.download = 'mercury-download';
+      }
       reader.readAsArrayBuffer(f)
     }
   }
 
-  decrypt(){
+  decrypt() {
     const pass = this.form2.value.password
     const reader = new FileReader()
-    const toast = (msg: string, title: string) => {showToast(this.toastService, msg, title, 'danger')}
+    const toast = (msg: string, title: string) => {
+      showToast(this.toastService, msg, title, 'danger')
+    }
     // @ts-ignore
     let f = document.getElementById('upload_cover').files[0]
     if (f) {
